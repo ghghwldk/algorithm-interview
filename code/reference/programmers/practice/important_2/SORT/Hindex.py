d671007f27a9ce04d20022ac986e36e9838f297a
ghghwldk <ghghwldk@gmail.com>
2024-02-16 10:02:23 +0900

    기존 코드 제거(책내용)

--- code/reference/programmers/practice/important_2/SORT/Hindex.py
+++
@@ -1,9 +0,0 @@
-def solution(citations):
-    citations.sort(reverse=True)
-    h_index = 0
-    for i, citation in enumerate(citations):
-        if citation >= i + 1:
-            h_index = i + 1
-        else:
-            break
-    return h_index
